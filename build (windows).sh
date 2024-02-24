a95f719b6453ed5a694f3e045a59aa1280f3d945
GavYeah <161093496+gavyeah8616@users.noreply.github.com>
2024-02-23 20:02:10 -0500

    build (windows).sh

---
+++ build (windows).sh
@@ -0,0 +1,3 @@
+nasm -f bin C:\(location it i in)\(Your file)\kernel.asm\ -o kasm.o
+copy kasm.o (Your file)\build\
+copy \b C:\(location it is in)\(Your file)\kernel.c\ C:\(location it is in)\(Your file)\build\kernel.o\
