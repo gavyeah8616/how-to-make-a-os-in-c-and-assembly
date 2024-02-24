d48a06ae9c4b8699df6525f744a39d73ca06e110
GavYeah <161093496+gavyeah8616@users.noreply.github.com>
2024-02-23 19:52:45 -0500

    build (linux commands).sh
    make sure to go into properties and run it as a program

--- build (linux).sh
+++ build (linux).sh
@@ -1,6 +1,6 @@
 #nasm -f elf32 kernel.asm -o kasm.o
 gcc -m32 -c kernel.c -o kc.o
-ld -m elf_i386 -t link.ld -o safe computer/boot/kernel.bin kasm.o kc.o
+ld -m elf_i386 -t link.ld -o (Your os file)/boot/kernel.bin kasm.o kc.o
 qemu -kernel safe computer/boot/kernel.bin
 #grub-mkrescue -o safe computer.iso safe computer/
 
